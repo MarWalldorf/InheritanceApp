4d818774b1c7d072e2fb086db72fdca67720c41b
MarWalldorf <mw305420@student.polsl.pl>
2023-12-23 13:55:13 +0100

    initial commit

---
+++ src/Bird.java
@@ -0,0 +1,23 @@
+public class Bird extends Animal {
+    private int legs;
+    private int eyes;
+    private int wings;
+    public Bird(String name, int brain, int size, int weight, int legs, int eyes, int wings) {
+        super(name, brain, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.wings = wings;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public int getWings() {
+        return wings;
+    }
+}
